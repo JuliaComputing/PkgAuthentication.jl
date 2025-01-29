524f4923b4cf326606626e303d4231896b843629
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2025-01-29 20:24:42 +0200

    feat(wsl): detect wsl

---
+++ src/helpers.jl
@@ -0,0 +1,7 @@
+# Vendored from Pluto.jl#src/notebook/path helpers.jl
+# from https://github.com/JuliaLang/julia/pull/36425
+function detectwsl()
+    Sys.islinux() &&
+    isfile("/proc/sys/kernel/osrelease") &&
+    occursin(r"Microsoft|WSL"i, read("/proc/sys/kernel/osrelease", String))
+end
