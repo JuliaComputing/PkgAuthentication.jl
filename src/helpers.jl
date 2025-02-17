05b5657ae2e0e396bc1f035b9fbc85d90e91fcb8
Morten Piibeleht <morten.piibeleht@juliahub.com>
2025-02-17 14:58:25 +1300

    borrow detectwsl implementation from Julia

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,7 +1,14 @@
-# Vendored from Pluto.jl#src/notebook/path helpers.jl
-# from https://github.com/JuliaLang/julia/pull/36425
-function detectwsl()
-    Sys.islinux() &&
-    isfile("/proc/sys/kernel/osrelease") &&
-    occursin(r"Microsoft|WSL"i, read("/proc/sys/kernel/osrelease", String))
+@static if isdefined(Sys, :detectwsl)
+    const detectwsl = Sys.detectwsl
+else
+    # Borrowed from Julia (MIT license)
+    # https://github.com/JuliaLang/julia/blob/726c816b9590d748345fb615b76b685c79eafd0d/base/sysinfo.jl#L549-L555
+    # https://github.com/JuliaLang/julia/pull/57069
+    function detectwsl()
+        # We use the same approach as canonical/snapd do to detect WSL
+        islinux() && (
+            isfile("/proc/sys/fs/binfmt_misc/WSLInterop")
+            || isdir("/run/WSL")
+        )
+    end
 end
